60b973399f43ebeacc0e11b22c4439936941d640
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-20 17:12:43 -0300

    Update jsEntradaSalida-9.js

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,24 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+	var sueldo1;
+	var resultado1;
+
+
+	sueldo1=importe.value;
+	resultado1=resultado.value;
+
+
+	sueldo1=parseInt(sueldo1);
+	resultado1=parseInt(resultado1);
+
+
+	var importeconaumento=sueldo1 - sueldo1/4;
+
+
+	document.getElementById('resultado').value=importeconaumento;
+	
+}
+
 	
 }
