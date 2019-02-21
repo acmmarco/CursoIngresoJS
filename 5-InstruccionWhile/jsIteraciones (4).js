b74eac0dda6018806b851316e869d4c88c82fbc1
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:15:56 -0300

    Update jsIteraciones (4).js

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+
+	var numeroingresado;
+
+	numeroingresado = prompt("ingrese un número entre 0 y 10.");
+
+	numeroingresado = parseInt(numeroingresado);
+
+	
+
+while (numeroingresado >= 9 && numeroingresado >= 0) 
+
+{
+	numeroingresado=prompt("Numero Incorrecto. Por Favor Reintentelo. Muchas Gracias");
+} 
+
+   Numero.value=numeroingresado;
 
 
-}//FIN DE LA FUNCIÓN
+  
+}//FIN DE LA FUNCIÓNv
