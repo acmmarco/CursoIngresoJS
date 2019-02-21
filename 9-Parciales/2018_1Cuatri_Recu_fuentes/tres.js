56b693439a3fe55a91710f1e5a3ef0765da2592c
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:50:37 -0300

    Update tres.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
 
+	var precio=prompt("Por Favor, Ingrese El Precio Sin Descuento: ");
+	precio=parseInt(precio);
+
+
+	var descuento=prompt("Por Favor, Ingrese El Valor del Descuento: ");
+	descuento=parseInt(descuento);
+
+
+	var preciofinal;
+	preciofinal=parseInt(preciofinal);
+
+
+	var preciofinal = precio - (precio * descuento/100);
+
+
+	document.getElementById('elPrecioFinal').value=preciofinal;
+
+
 }
