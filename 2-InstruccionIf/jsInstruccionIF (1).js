7e426835c1cbba4c898611311d25f2a4f4738f41
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 00:09:41 -0300

    Update jsInstruccionIF (1).js

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,18 @@ function mostrar()
 {
 //tomo la edad  
 
+var edadaingresar;
 
+   edadaingresar=document.getElementById('edad').value;
+
+   edadaingresar=parseInt(edadaingresar);
+   
+
+      if (edadaingresar == 15) 
+
+   	   {
+   		   alert("Sos una Niña Bonita :D");
+   	   }
+   
 
 }//FIN DE LA FUNCIÓN
