2ccf14094401ac58391907e92933266ec34d7194
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 00:30:31 -0300

    Update jsInstruccionIF (9).js

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	var numero;
+
+	numero=Math.round(Math.random()*10);
+
+	alert("El Numero Aleatorio Entre 1 y 10 es: " + numero);
+
 
 }//FIN DE LA FUNCIÓN
