5e40028a30f930a00f3e240da96c522f0fb59a65
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:25:55 -0300

    Update jsIteraciones (7).js

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,8 +5,44 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+	var sumar=0;
+
+
+    acumulador=suma.value;
+    
+    acumulador=promedio.value;
+    
+	
+	acumulador=prompt('Ingrese Nuevo valor: ');
+  	acumulador=parseInt(acumulador);
+
+    respuesta=prompt("Desea Ingresar Un Nuevo Valor???");
+
+
+
+while (respuesta == "si")
+
+{
+
+	acumulador=prompt('Ingrese Nuevo valor: ');
+  	acumulador=parseInt(acumulador);
+
+  	sumar = sumar + acumulador[i]; 
+			
+
+  	respuesta=prompt("Desea Ingresar Un Nuevo Valor???");
+
+  	
+
+	contador++;
+
+} 
+
+
+
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
+
 
 }//FIN DE LA FUNCIÓN
