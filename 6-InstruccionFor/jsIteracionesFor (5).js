ffe379fd13eb452d7077e1f377543cb542a75e99
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:32:53 -0300

    Update jsIteracionesFor (5).js

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,24 @@
 function mostrar()
+
 {
 
+var repeticiones;
+repeticiones=parseInt(repeticiones);
 
+
+for (var i=1; i<=100000; i++)
+
+		{ 
+				repeticiones=prompt("Ingrese Un Numero: ");
+
+
+				if (repeticiones =="9")
+				{ 
+					alert("No Se Ejecuta Mas :D")
+					break;	 
+	        	} 
+
+		} 
 
 
 }//FIN DE LA FUNCIÓN
