2f25220ac2f0908f03d6e08cd283e08af71db923
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:27:03 -0300

    Update jsIteraciones (8).js

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -8,6 +8,39 @@ function mostrar()
 	var respuesta='si';
 
 
+
+    acumulador=suma.value;
+    
+    acumulador=producto.value;
+
+
+	
+	acumulador=prompt('Ingrese Nuevo valor: ');
+  	acumulador=parseInt(acumulador);
+
+    respuesta=prompt("Desea Ingresar Un Nuevo Valor???");
+
+    
+
+while (respuesta == "si")
+
+{
+
+	acumulador=prompt('Ingrese Nuevo valor: ');
+  	acumulador=parseInt(acumulador);
+
+  	sumar = sumar + acumulador[i]; 
+			
+
+  	respuesta=prompt("Desea Ingresar Un Nuevo Valor???");
+
+  	
+
+	contador++;
+
+} 
+
+
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
 
