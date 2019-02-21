6f6371f3ef1714216b460c094f3206b7a8e8dd70
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:44:18 -0300

    Update cinco.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,36 @@
 function mostrar()
 {
 
+	var planeta=prompt("Por Favor, Ingrese Un Planeta: ");
+
+	switch (planeta)
+
+	{
+
+		case "tierra":
+			alert("Aca Vivimos");
+			break;
+
+    	case "mercurio":
+    	case "venus":
+    		alert("Aca Hace Mas Calor");
+    		break;
+
+    	case "marte":
+    	case "jupiter":
+    	case "saturno":
+    	case "urano":
+    	case "neptuno":
+    		alert("Aca Hace Mas Frio");
+    		break;
+
+    	default:	
+    		alert("Por Favor Ingrese Un Planeta Valido");
+    		break;
+
+	}
+
+
+
+
 }
