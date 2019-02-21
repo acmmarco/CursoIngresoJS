fa05bf589fc34c57a6b23c19d31a8deab6dd5a43
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 00:17:22 -0300

    Update jsInstruccionIF (4).js

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,38 @@ function mostrar()
 {
 //tomo la edad  
 
+var edadaingresar;
 
+   edadaingresar=document.getElementById('edad').value;
+
+   edadaingresar=parseInt(edadaingresar);
+
+
+      if (edadaingresar >= 18) 
+
+   	   {
+   		   alert("Usted Es Mayor de Edad");
+   	   }
+
+
+      else  
+
+   	   {
+   		   
+   		    if (edadaingresar >= 13) 
+
+   	          {
+   		           alert("Usted Es Un Adolescente");
+   	          }  
+
+
+   	        else 
+
+   	          {
+   		           alert("Usted NO Es Mayor de Edad");
+   	          }  
+
+   	   }   
+   	   
 
 }//FIN DE LA FUNCIÓN
