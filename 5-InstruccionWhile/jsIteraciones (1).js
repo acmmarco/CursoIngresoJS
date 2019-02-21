fc10bd5dd5ba7fa475c309980bc4df79d4c0ac2e
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:08:09 -0300

    Update jsIteraciones (1).js

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	alert('iteración while');
+	
+	var contador;
+
+	contador=0;
+
+	while (contador<=10)
+  
+    {
+    	alert("Iteración While: " + contador);
+        contador=contador+1;
+    }
+
 
 
 }//FIN DE LA FUNCIÓN
