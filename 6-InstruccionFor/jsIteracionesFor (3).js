8d9bf64cb5e33b0c825e1ebc0e6161835229e03f
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:31:03 -0300

    Update jsIteracionesFor (3).js

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,18 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
 
+var repeticiones = prompt("Ingrese El Número De Repeticiones");
+
+repeticiones=parseInt(repeticiones);
+
+
+
+for (var i=1; i<=repeticiones; i++)
+
+		{ 
+				alert("Hola UTN FRA"); 
+		} 
 
 
 }//FIN DE LA FUNCIÓN
