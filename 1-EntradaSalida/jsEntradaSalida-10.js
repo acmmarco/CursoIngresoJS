0db8de3979f3fb9227429b2b4535c3e82dbcad4a
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 00:03:32 -0300

    Update jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+
+	var sueldo1;
+	var resultado1;
+
+
+	sueldo1=importe.value;
+	resultado1=resultado.value;
+
+
+	sueldo1=parseInt(sueldo1);
+	resultado1=parseInt(resultado1);
+
+
+	var importeconaumento=sueldo1 - sueldo1/4;
+
+
+	document.getElementById('resultado').value=importeconaumento;
 	
 }
