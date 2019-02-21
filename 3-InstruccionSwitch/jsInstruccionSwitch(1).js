03ee2dd03c6a928a2140bebed476aa77da8bc2cb
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 00:34:36 -0300

    Update jsInstruccionSwitch(1).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,40 @@
 function mostrar()
 {
 //tomo la edad  
+
 var mesDelAño = document.getElementById('mes').value;
 
+mesDelAño=mes.options[mes.selectedIndex].value;
 
+
+switch (mesDelAño)
+
+{ 
+	case "Enero":
+	{ 
+		alert("Que Comiences Bien El Año!!! :D");
+		break;
+	}
+
+	case "Marzo":
+	{ 
+		alert("A Claaaseees!!!");
+		break;
+	}
+
+	case "Julio":
+	{ 
+		alert("Se Vienen Las Vacaciones!!! :D");
+		break;
+	}
+
+	case "Diciembre":
+	{ 
+		alert("Felices Fiestas!!! :D");
+		break;
+	}
+
+}
 
 
 }//FIN DE LA FUNCIÓN
