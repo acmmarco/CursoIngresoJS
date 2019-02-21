4e278ff8b332a1c31db69fe6445e68719913dc6a
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:37:04 -0300

    Update dos.js

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
-  
+
+  var nombre=document.getElementById('elNombre').value;
+
+  var localidad=document.getElementById('laLocalidad').value;
+
+
+  alert("Usted Es " + nombre + " Y Vive En La Localidad De: " + localidad);
+
+
 }
