4fcece5468cc19c3f61a615c19e2e3da615d35e2
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 00:25:34 -0300

    Update jsInstruccionIF (7).js

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,38 @@ function mostrar()
 {
 //tomo la edad  
 
-	
 
+var edadaingresar;
+var esposas;
+
+
+   edadaingresar=document.getElementById('edad').value;
+   esposas=estadoCivil.options[estadoCivil.selectedIndex].value;
+
+
+   edadaingresar=parseInt(edadaingresar);
+  
+
+
+      if (edadaingresar <= 17 && esposas == "Casado") 
+
+   	   {
+   		   alert("Es Muy Pequeño/a para NO ser Soltero/a");
+   	   }
+
+
+      else  
+
+   	   {
+   		   
+   		    if (edadaingresar <= 17 && esposas == "Divorciado") 
+
+   	          {
+   		           alert("Es Muy Pequeño/a para NO ser Soltero/a");
+   	          }  
+
+   	   }   
+   	   
+         
 
 }//FIN DE LA FUNCIÓN
