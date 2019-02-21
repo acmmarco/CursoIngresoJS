a740d24e2cef07bd899aafb96433fdccb95fe614
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:48:31 -0300

    Update uno.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -2,4 +2,16 @@
 function mostrar()
 {
 
+	var ancho=prompt("Por Favor, Ingrese el Ancho de Un Rectangulo (en cms): ");
+	ancho=parseInt(ancho);
+
+	var alto=prompt("Por Favor, Ingrese el Alto de Un Rectangulo (en cms) : ");
+	alto=parseInt(alto);
+
+	var perimetro= (ancho * 2) + (alto * 2);
+
+	alert("El Perimetro Del Rectangulo Es De: "+ perimetro + " cms.");
+
 }
+
+
