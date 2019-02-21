704bf891622f47393193713be4bbf3ac1047bb17
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-21 07:04:22 -0300

    Update jsInstruccionSwitch(4).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,45 @@
 function mostrar()
 {
 //tomo la edad  
+
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+mesDelAño=mes.options[mes.selectedIndex].value;
+
+// alert (mesDelAño);
+
+
+switch (mesDelAño)
+
+{ 
+	case "Enero":
+	case "Marzo":
+	case "Mayo":
+	case "Julio":
+	case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+	{ 
+		alert("Este Mes Tiene 31 Dias");
+		break;
+	}
+    
+	case "Abril":
+	case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+	{ 
+		alert("Este Mes Tiene 30 Dias");
+		break;
+	}
 	
-	
+    case "Febrero":
+	{ 
+		alert("Este Mes Tiene 28 Dias");
+		break;
+	}
+
+}
 
 
 
