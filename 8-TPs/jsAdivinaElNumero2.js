e07013f31a313c33234e90e0fd122b3dccc2ee4b
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-22 06:52:31 -0300

    Update jsAdivinaElNumero2.js

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -10,19 +10,107 @@ Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
+numeroSecreto=parseInt(numeroSecreto);
+
 var contadorIntentos;
+contadorIntentos=parseInt(contadorIntentos);
+
+var numeroingresado;
+numeroingresado=parseInt(numeroingresado);
+
+
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
+
+    numeroSecreto=Math.floor(Math.random()*100)+1;
+    contadorIntentos=0;
 
 }
+
+
 
 function verificar()
 {
-	
-	
+
+    contadorIntentos=contadorIntentos+1;
+
+    numero.value=numeroingresado;
+
+    intentos.value=contadorIntentos;
+
+    console.log(numeroSecreto);
+
+
+    if (numeroingresado == numeroSecreto)
+
+      {
+          alert("Usted Es El Ganadooor!!!, Y Solo En " + contadorIntentos + " Intentos.");
+      }
+
+    else 
+
+      {
+          if (numeroingresado < numeroSecreto)
+
+          {
+              alert("Falta... :D");
+          }
+
+
+          else 
+
+          {
+              alert("Te Pasaste... :D");
+          }
+
+      }
+
 
 }
+
+
+
+	switch (contadorIntentos)
+
+	{    
+		case "1":
+		{    
+			alert("Usted Es Un Psiquico");
+	    }
+
+	    case "2":
+		{    
+			alert("Excelente Percepcion");
+	    }
+
+	    case "3":
+		{    
+			alert("Esto Es Suerte");
+	    }
+	    case "4":
+		{    
+			alert("Excelente Tecnica");
+	    }
+
+        case "5":
+		{    
+			alert("Usted Esta En La Media");
+	    }
+
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "10":
+		{    
+			alert("Falta Tecnica");
+	    }
+
+	    default:
+		{    
+			alert("Afortunado En El Amor :D");
+	    }
+
+
+	}
