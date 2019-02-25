e39eea4e736102c35744cf57b285db26914d3ab0
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-25 07:44:44 -0300

    Update jsPiedarPapelTijera2.js

--- 8-TPs/jsPiedarPapelTijera2.js
+++ 8-TPs/jsPiedarPapelTijera2.js
@@ -1,31 +1,120 @@
+
 var eleccionMaquina;
+var eleccionusuario;
 var ContadorDeEmpates=0;
 var ContadorDeGanadas=0;
 var ContadorDePerdidas=0;
 
+
+
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 3
-	 	
+
+  eleccionMaquina = (Math.floor(Math.random() * 3) + 1);
+
+
+  eleccionusuario=parseInt(eleccionusuario);
+
+
+
+  switch (eleccionMaquina)
+  {
+        case 1:
+        {
+            console.log("La Maquina Eligio Piedra");
+            break;
+        }
+
+         case 2:
+        {
+            console.log("La Maquina Eligio Papel");
+            break;
+        }
+
+         case 3:
+        {
+            console.log("La Maquina Eligio Tijera");
+            break;
+        }
+  }
+
 
 
 
 }//FIN DE LA FUNCIÓN
+
+
+
+  
+
 function piedra()
 {
 	
+    if (eleccionMaquina == eleccionusuario) 
+    {
+      	alert("Es Un Empate!!! Los Dos Eligieron Lo Mismo ");
+        ContadorDeEmpates++;
+   	}
+ 
+    else 
+    {
+        if ((eleccionusuario == 1) && (eleccionMaquina == 2)) 
+        {
+            alert("Gana La Maquina.");
+            ContadorDePerdidas++;
+        } 
 
-}//FIN DE LA FUNCIÓN
-function papel()
-{
+        else ((eleccionusuario == 1) && (eleccionMaquina == 3)) 
+        {
+            alert("Gana El Usuario :D.");
+            ContadorDeGanadas++;
+        } 
 
-}//FIN DE LA FUNCIÓN
-function tijera()
-{
+    } 
 	
 }//FIN DE LA FUNCIÓN
 
-function mostrarResultado()
+
+
+function papel()
 {
 
+       if ((eleccionusuario == 2) && (eleccionMaquina == 3)) 
+        {
+            alert("Gana La Maquina.");
+            ContadorDePerdidas++;
+        } 
+
+        else ((eleccionusuario == 2) && (eleccionMaquina == 1)) 
+        {
+            alert("Gana El Usuario :D.");
+            ContadorDeGanadas++;
+        } 
+
+}//FIN DE LA FUNCIÓN
+
+
+function tijera()
+{
+
+       if ((eleccionusuario == 3) && (eleccionMaquina == 2)) 
+        {
+            alert("Gana La Maquina.");
+            ContadorDePerdidas++;
+        } 
+
+        else ((eleccionusuario == 3) && (eleccionMaquina == 1)) 
+        {
+            alert("Gana El Usuario :D.");
+            ContadorDeGanadas++;
+        } 
+
+}//FIN DE LA FUNCIÓN
+
+
+function mostrarResultado()
+{
+	document.getElementById("ganadas").value=ContadorDeGanadas;
+	document.getElementById("empatadas").value=ContadorDeEmpates;
+	document.getElementById("perdidas").value=ContadorDePerdidas;
 }
