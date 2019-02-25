1619efc4ea610f63b6531d967e027b265f377152
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-25 08:15:50 -0300

    Update jsIteracionesFor (6).js

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 
+  var numero;
+  numero=prompt("Por Favor Ingrese Un Numero ");
+
+  var inicializador;
+
+
+  for (inicializador=2; inicializador<=numero; inicializador+=2)
+  {    
+      console.log("Numero: " + inicializador);
+  }
 
 
 
