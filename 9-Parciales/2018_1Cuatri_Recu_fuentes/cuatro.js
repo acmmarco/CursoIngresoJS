a09cd321bf1a45e6a484734253b8b52920f9fe59
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-25 21:06:03 -0300

    Update cuatro.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,54 @@
 function mostrar()
+
 {
+
+	var numero1=prompt("Por Favor, Ingrese El Numero 1: ");
+	numero1=parseInt(numero1);
+
+	var numero2=prompt("Por Favor, Ingrese El Numero 2: ");
+	numero2=parseInt(numero2);
+
+	var resultado;
+
+	if (numero1 == numero2)
+
+		{
+			alert("Los Numeros Son: " + numero1 + " y " + numero2);
+		}
+
+	else 
+
+		{
+			if (numero1 > numero2)
+
+			{
+				resultado=numero1 / numero2;
+				alert("El Resultado De La Resta Entre: " + numero1 + " y " + numero2 + " Es: " + resultado);
+			}
+			
+			else 
+
+		    {
+			    
+			    if (numero1 < numero2)
+
+				{
+					resultado=numero1+numero2;
+                    alert("El Resultado De La Suma Entre: " + numero1 + " y " + numero2 + " Es: " + resultado);		
+						
+
+						if (resultado > "50")
+
+							{
+								resultado=numero1+numero2;
+								alert("La Suma Es: " + resultado + " Y Supero el 50");
+							}
+
+				}
+
+		    }
+
+
+		}
 
 }
