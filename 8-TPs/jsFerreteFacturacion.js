7b831d2db6ce393e53357c9fad63097cc8533b9f
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-25 22:28:00 -0300

    Update jsFerreteFacturacion.js

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,88 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+
+
+function Sumar () // CUIDADO!!! Puede Ser Que No Ande Porque Hay Un Error En El ONCLICK Del HTML 
+{   
+
+    var preone;
+    var pretwo;
+    var prethree;
+    var sumar;
+
+
+    preone=PrecioUno.value;
+    pretwo=PrecioDos.value;
+    prethree=PrecioTres.value;
+
+    
+    preone=parseInt(preone);
+    pretwo=parseInt(pretwo);
+    prethree=parseInt(prethree);
+
+
+    sumar=preone+pretwo+prethree;
+    alert("La Suma Es: " + sumar);
+
 }
+
+
+
 function Promedio () 
 {
+
+    var preone;
+    var pretwo;
+    var prethree;
+    var sumar;
+    var promedio;
+
+
+    preone=PrecioUno.value;
+    pretwo=PrecioDos.value;
+    prethree=PrecioTres.value;
+
+    
+    preone=parseInt(preone);
+    pretwo=parseInt(pretwo);
+    prethree=parseInt(prethree);
+
+
+    sumar=preone+pretwo+prethree;
+    promedio=sumar / 3;
+
+
+    alert("El Promedio Es: " + promedio);
 	
 }
+
+
 function PrecioFinal () 
 {
-	
+    
+    var preone;
+    var pretwo;
+    var prethree;
+    var sumar;
+    var precioiva;
+    
+
+
+    preone=PrecioUno.value;
+    pretwo=PrecioDos.value;
+    prethree=PrecioTres.value;
+
+    
+    preone=parseInt(preone);
+    pretwo=parseInt(pretwo);
+    prethree=parseInt(prethree);
+
+
+    sumar=preone+pretwo+prethree;
+    precioiva=sumar+(sumar*0.21);
+ 
+ 
+    alert("El Precio Con El Iva Incluido Es: " + precioiva);
+    
 }
