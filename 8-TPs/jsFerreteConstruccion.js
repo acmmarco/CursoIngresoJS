6a07db2ef6ce5456f7ef496ca8fad4546512c752
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-25 23:13:06 -0300

    Update jsFerreteConstruccion.js

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -7,12 +7,87 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+	var largo;
+	var ancho;
+	var perimetro;
+	var perimetro3;
+
+
+	largo=Largo.value;
+	ancho=Ancho.value;
+
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+
+	perimetro=(largo*2)+(ancho*2);
+	perimetro3=perimetro*3;
+
+
+	alert("3 Rectangulos De Alambre: "+perimetro3);
+
+
 }
+
+
+
+
 function Circulo () 
 {
+
+	var radio;
+	var diametro;
+	var perimetro;
+	var perimetro3;
+
+
+	radio=Radio.value;
+
+
+	radio=parseInt(radio);
+
+
+    diametro=radio*2;
+	perimetro= Math.PI * diametro;
+	perimetro3=perimetro * 3;
+
+
+	alert("3 Circulos De Alambre: "+perimetro3);
 	
+
 }
+
+
+
+
 function Materiales () 
 {
+
+	var largo;
+	var ancho;
+	var area;
+	var bolsacemento;
+	var bolsacal;
+
+
+	largo=Largo.value;
+	ancho=Ancho.value;
+
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+
+
+	area=largo*ancho;
+
+
+	bolsacemento=area*2;
+	bolsacal=area*3;
+
+
+	alert("Materiales Contrapiso: Por Un Area De " + area + " m2 Se Necesitan " 
+	+ bolsacemento + " Bolsas De Cemento Y " + bolsacal + " Bolsas De Cal.");
+
 	
 }
