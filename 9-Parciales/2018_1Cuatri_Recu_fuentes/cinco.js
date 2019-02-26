ace71c66a9794b1deacd400e3d122a957c543e09
Antonella <47437165+acmmarco@users.noreply.github.com>
2019-02-25 21:16:32 -0300

    Update cinco.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,42 @@
+
 function mostrar()
 {
+
+	var dia=prompt("Por Favor, Ingrese Un Dia De La Semana: ");
+
+	var mensaje;
+
+
+	switch (dia)
+	{
+		case "lunes":
+		case "martes":
+		case "miercoles":
+		case "jueves":
+		case "viernes":
+		{
+			mensaje="A Trabajaaar!!! :-I";
+			break;
+		}
+
+		case "sabado":
+		case "domingo":
+		{
+			mensaje="Buen Findeee!!! :-D";
+			break;
+		}
+
+		default:
+		{
+			mensaje="Por Favor, Ingrese Un Dia De La Semana Valido. Muchas Gracias.";
+			break;
+		}
+
+
+	}
+
+
+	alert(mensaje);
+
 
 }
